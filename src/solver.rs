0dc6af8d7904bf3f0527397594ee1c97b32a9dad
Noah Wahl <noah.ares@yahoo.de>
2023-01-21 17:07:35 +0100

    Fix variable generation
    Model looks good

--- src/solver.rs
+++ src/solver.rs
@@ -9,13 +9,13 @@ pub fn solve(dataframe: &Dataframe, num_cores: usize) -> Result<Array1::<grb::Va
     let mut model = Model::new("portfolio_model")?;
     let (n, m) = (dataframe.algorithms.len(), dataframe.instances.len());
 
-    let a = Array3::<grb::Var>::from_elem((m, n, num_cores), add_binvar!(model)?);
-    let b = Array2::<grb::Var>::from_elem((n, num_cores), add_binvar!(model)?);
-    let assigned_resources_per_algo = Array1::<grb::Var>::from_elem(n, add_ctsvar!(model, bounds: 0..num_cores)?);
-    let q = Array1::<grb::Var>::from_elem(n, add_ctsvar!(model, bounds: ..)?);
+    let a = Array3::<grb::Var>::from_shape_fn((m, n, num_cores), |(i, j, k)| add_binvar!(model, name: format!("a_{i}_{j}_{k}").as_str()).unwrap());
+    let b = Array2::<grb::Var>::from_shape_fn((n, num_cores), |(j, k)| add_binvar!(model, name: format!("b_{j}_{k}").as_str()).unwrap());
+    let assigned_resources_per_algo = Array1::<grb::Var>::from_shape_fn(n, |j| add_ctsvar!(model, name: format!("k_{j}").as_str(), bounds: 0..num_cores).unwrap());
+    let q = Array1::<grb::Var>::from_shape_fn(m, |i| add_ctsvar!(model, name: format!("q_{i}").as_str(), bounds: ..).unwrap());
     let best_per_instance = Array1::<f64>::ones(n);
 
-    let e_min = Array3::<f64>::zeros([m, n, num_cores]);
+    let e_min = Array3::<f64>::ones([m, n, num_cores]);
 
     // constraint 1
     let c_1 = a.indexed_iter()
@@ -33,10 +33,10 @@ pub fn solve(dataframe: &Dataframe, num_cores: usize) -> Result<Array1::<grb::Va
         .into_iter()
         .map(|row| row.into_iter().zip(assigned_resources_per_algo.iter()).map(|(var, k)| *var * *k).grb_sum())
         .grb_sum();
-    let c_3 = model.add_constr("c3", c!(sums == num_cores));
+    let c_3 = model.add_qconstr("c3", c!(sums == num_cores));
 
     // constraint 4
-    let c_4 = a.rows()
+    let c_4 = a.outer_iter()
         .into_iter().enumerate()
         .map(|(i, row)| model.add_constr(format!("c4_{i}").as_str(), c!(row.iter().grb_sum() == 1)))
         .collect_vec();
