e679bae1e625968daf95e5005c671a1a96ab0971
Noah Wahl <noah.ares@yahoo.de>
2023-01-29 11:21:56 +0100

    Fix debug formatting

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,11 @@ fn main() -> Result<()> {
     let config_path = env::args().nth(1).expect("No json config provided!");
     let config_str = fs::read_to_string(config_path)
         .expect("Provided config file does not exist");
-    let config: Config = serde_json::from_str(&config_str)?;
+    let config: Config = serde_json::from_str(&config_str)
+        .expect("Error while reading config file");
     let data = Data::new(&config.files, config.num_cores)?;
     let result = solver::solve(&data, config.num_cores as usize)?;
-    dbg!("assignment {:?}", result);
-    dbg!("{:?}", data.algorithms);
+    dbg!(result);
+    dbg!(data.algorithms);
     Ok(())
 }
